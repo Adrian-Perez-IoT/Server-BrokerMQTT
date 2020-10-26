b669324f3868d2162d51fbbdcc54c8b390efdba5
Adrian <perez.adrian.matias@gmail.com>
2020-10-26 17:44:07 -0300

    algo

--- src/broker.js
+++ src/broker.js
@@ -15,28 +15,28 @@ server.listen(port, function () {
 
 
 
- // aedes.on('clientError', function (client, err) {
-//   console.log('client error', client.id, err.message, err.stack)
-// })
+ aedes.on('clientError', function (client, err) {
+  console.log('client error', client.id, err.message, err.stack)
+})
 
-// aedes.on('connectionError', function (client, err) {
-//   console.log('client error', client, err.message, err.stack)
-// })
+aedes.on('connectionError', function (client, err) {
+  console.log('client error', client, err.message, err.stack)
+})
 
-// aedes.on('publish', function (packet, client) {
-//   if (client) {
-//     console.log('message from client', client.id)
-//   }
-// })
+aedes.on('publish', function (packet, client) {
+  if (client) {
+    console.log('message from client', client.id)
+  }
+})
 
-// aedes.on('subscribe', function (subscriptions, client) {
-//   if (client) {
-//     console.log('subscribe from client', subscriptions, client.id)
-//   }
-// })
+aedes.on('subscribe', function (subscriptions, client) {
+  if (client) {
+    console.log('subscribe from client', subscriptions, client.id)
+  }
+})
 
-// aedes.on('client', function (client) {
-//   console.log('new client', client.id)
-// })
+aedes.on('client', function (client) {
+  console.log('new client', client.id)
+})
 
-// console.log(`Environment: ${process.env.NODE_ENV}`);a
+console.log(`Environment: ${process.env.NODE_ENV}`);a
