d0850aeb1e1e7aca6285820191bd9cf3abefbfe5
Adrian <perez.adrian.matias@gmail.com>
2020-11-06 21:00:56 -0300

    Se agrego la funcionalidad de un servidor express y config de notificacion push al dispositivo movil

---
+++ src/notifications.js
@@ -0,0 +1,47 @@
+const admin = require("firebase-admin");
+
+function initFirebase () {
+    const serviceAccount = require("../key/testnotificationpushflutter-firebase-adminsdk-614sv-b391a4e5b1.json");
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: "https://testnotificationpushflutter.firebaseio.com"
+      });    
+};
+
+initFirebase();
+
+function sendPushToOneUser(notification) {
+  const message = {
+      token: notification.tokenId,
+      data: {
+          titulo: notification.titulo,
+          mensaje: notification.mensaje
+      }
+  }
+  sendMessage(message);
+}
+
+function sendPushToTopic(notification) {
+  const message = {
+      topic: notification.topic,
+      data: {
+          titulo: notification.titulo,
+          mensaje: notification.mensaje
+      }
+  }
+  sendMessage(message);
+}
+
+module.exports = { sendPushToOneUser, sendPushToTopic }
+
+function sendMessage(message) {
+  admin.messaging().send(message)
+      .then((response) => {
+          // Response is a message ID string.
+          console.log('Successfully sent message:', response);
+      })
+      .catch((error) => {
+          console.log('Error sending message:', error);
+      })
+}
+
---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const Notification = require ("./notifications.js");
+// const GenerateToken = require("./generateToken.js") // only needed if you want to generate your own tokens
+
+const app = express();
+
+app.get("/one-user", function(req,res){
+    res.send("sending notification");
+    const data = {
+        tokenId: "esXH_nLk5F0:APA91bGIPzBuU0yAHAe2wfLlGZYEhfuMO0o8sHDb3CAsr1ZzcnPmIcqrZwP4mSvX5aB2qoLYLgG7W2_A-iaE0gq4m0qSXyYY2gKAWPLxw9aK6wm058-jdYGXyuGrLnbt-PF8YhxjPx9l",
+        titulo: "myTitulo",
+        mensaje: "mymensaje"
+    };
+    Notification.sendPushToOneUser(data);    
+});
+
+app.get("/", function(req, res) {
+    res.send("Respuesta de la peticion get. Satisfactorio")
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
