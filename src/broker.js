64e7587633f3adb867be52bf2165a35941c5b5e5
Adrian <perez.adrian.matias@gmail.com>
2020-11-10 00:54:39 -0300

    Se alcanzo el objetivo de notificar a la app cuando el sensor pir detecta movimiento

--- src/broker.js
+++ src/broker.js
@@ -24,18 +24,18 @@ aedes.on('publish', function (packet, client) {
   if (client) {
     //aqui la logica para identificar si el mensaje publicado es una posible amenaza
     /* console.log('Message from client', client.id); */
-
+    //agregar logia switch case --> topic motion, door, gas. 
     if (packet.topic.toString() == "Home/LivingRoom/Motion001") {
-      console.log('Se recibio una publicacion en el topic:', packet.topic, ' Con el mensaje: ', packet.payload.toString()); // Convertir en JSON el paload. evaluarlo (identif sensor). Y notificar (llamar a la funcion) a la app VigBee ().
-
-      
+      // console.log('Se recibio una publicacion en el topic:', packet.topic, ' Con el mensaje: ', packet.payload.toString()); // Convertir en JSON el paload. evaluarlo (identif sensor). Y notificar (llamar a la funcion) a la app VigBee ().      
       let amenaza = JSON.parse(packet.payload.toString()); //amenaza debe ser un JSON. ¿Como lo creo?
-      amenaza["tokenId"] = "esXH_nLk5F0:APA91bGIPzBuU0yAHAe2wfLlGZYEhfuMO0o8sHDb3CAsr1ZzcnPmIcqrZwP4mSvX5aB2qoLYLgG7W2_A-iaE0gq4m0qSXyYY2gKAWPLxw9aK6wm058-jdYGXyuGrLnbt-PF8YhxjPx9l";
-      amenaza["titulo"] = "Alerta de posible amenaza";
-      amenaza["mensaje"] = "Movimientos detectado recientemente en tu casa";
-      // convierte en JSON. (Deserializar). Es requisito para enviar un json al VigBee   
-      console.log('La amenaza convertido en JSON es (prety): ', amenaza);
-      // Notification.sendAlert(amenaza);// ERROR AQUI . CORREGIR MAÑANA: --> DATA DEBE CONETENR UNICAMENTE CALORES STRING
+      if (amenaza["value"] == "true") {
+        // añadir la logica para que se notifique cada minuto (para no hacerlo cada 5 segundos que saturarà la app vigbee/Backend FCM/ Backend MQTT?
+        console.log('Se notificará la amenaza de movimiento:', amenaza);
+        amenaza["tokenId"] = "esXH_nLk5F0:APA91bGIPzBuU0yAHAe2wfLlGZYEhfuMO0o8sHDb3CAsr1ZzcnPmIcqrZwP4mSvX5aB2qoLYLgG7W2_A-iaE0gq4m0qSXyYY2gKAWPLxw9aK6wm058-jdYGXyuGrLnbt-PF8YhxjPx9l";
+        amenaza["titulo"] = "Alerta de posible amenaza";
+        amenaza["mensaje"] = "Movimiento detectado";
+        Notification.sendAlert(amenaza);// ERROR AQUI . CORREGIR MAÑANA: --> DATA DEBE CONETENR UNICAMENTE CALORES STRING        
+      }
     }
 
     // data = JSON.parse(packet.payload.toString()); // para experimentar si se puede o no.
